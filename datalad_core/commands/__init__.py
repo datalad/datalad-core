60f56388fd5bef78de4d3e2b7234219c27ac37ff
Michael Hanke <michael.hanke@gmail.com>
2024-10-16 20:23:47 +0200

    feat: `@datalad_command` decorator
    This decorator can equip a function with parameter validation and
    result post-processing. It replaces fully replaces the command interface
    used in legacy DataLad, where a combination of a class with a number of
    static methods, and multiple decorators have been used to achieve
    a similar goal, but at the expense of a substantial amount of
    boilerplate code.
    
    Importantly, result post-processing is fully factored out into a
    `ResultHandler`. This means that the typical behavior can
    configurability of a datalad command is now fully defined by a result
    handler (class), which is now configurable/replaceable. In contract to
    legacy Datalad, this even allows for commands that are not generators
    of result records, and that may not make sense to run via a CLI.
    
    A `PassthroughHandler` and a `DefaultHandler` are included as examples.
    The latter is structurally identical to the result handling
    implementation used in legacy Datalad (minus result hook support).
    It is, however, incomplete in that only the flow control (`on_failure`)
    is actually implemented.

---
+++ datalad_core/commands/__init__.py
@@ -0,0 +1,53 @@
+"""Components for implementing DataLad commands
+
+At the very core, a DataLad command is function that documents and validates
+its parameters in a particular way, and yields return values that follow
+particular conventions.
+
+In its simplest form, a DataLad command can be created like this:
+
+    >>> @datalad_command()
+    ... def my_command(some_arg):
+    ...     # do processing ...
+    ...     return  # typically one or more results are returned/yielded
+
+The :func:`datalad_command` decorator wraps a function, and automatically
+establishes the conventions expected for a DataLad command (see its
+documentation for details).
+
+Beyond this simplest use, the decorator can be used to customize parameter
+validation, and result handling. Both aspects are handled by customizable
+classes.
+
+.. currentmodule:: datalad_core.commands
+.. autosummary::
+   :toctree: generated
+
+   datalad_command
+   ResultHandler
+   StandardResultHandler
+   PassthroughHandler
+   get_default_result_handler
+   set_default_result_handler
+"""
+
+__all__ = [
+    'StandardResultHandler',
+    'ResultHandler',
+    'PassthroughHandler',
+    'datalad_command',
+    'get_default_result_handler',
+    'set_default_result_handler',
+]
+
+
+from .decorator import datalad_command
+from .default_result_handler import (
+    StandardResultHandler,
+    get_default_result_handler,
+    set_default_result_handler,
+)
+from .result_handler import (
+    PassthroughHandler,
+    ResultHandler,
+)

7da8bd436b8ec31c483917aad022fcd275519613
Michael Hanke <michael.hanke@gmail.com>
2024-10-17 18:16:22 +0200

    feat: foundational classes for constraints
    The basic concepts have been handed down from our forefathers, and are
    already used in legacy DataLad.
    
    This implementation is derived from the one in datalad-next. The main
    difference to legacy DataLad is the key helper `Constraint.raise_for()`
    which standardized error generation.
    
    The main difference to the implementation in datalad-next is the removal
    of any legacy compatibility shims.

--- datalad_core/constraints/__init__.py
+++ datalad_core/constraints/__init__.py
@@ -1,5 +1,21 @@
 """Parameter validation, coercion, and documentation
 
+This module provides a set of classes to validate and document
+parameters. In a nutshell, each of these
+:class:`Constraint` classes:
+
+- focuses on a specific aspect, such as type coercion,
+  or checking particular input properties
+- is instantiated with a set of parameters to customize
+  such an instance for a particular task
+- performs its task by receiving an input via its ``__call__()``
+  method
+- provides default auto-documentation
+
+Individual :class:`Constraint` instances can be combined with logical ``and``
+(:class:`AllOf`) and ``or`` (:class:`AnyOf`) operations to form arbitrarily
+complex constructs.
+
 For (validation/coercion) errors, a :class:`ConstraintError` class is provided.
 This class supports error reporting in a structure fashion with standard (yet
 customizable) error messages, and is capable of communicating the underlying
@@ -10,14 +26,25 @@ descriptions.
 .. autosummary::
    :toctree: generated
 
+   Constraint
+   AllOf
+   AnyOf
    ConstraintError
 """
 
 __all__ = [
+    'Constraint',
+    'AllOf',
+    'AnyOf',
     'ConstraintError',
 ]
 
 
+from .constraint import (
+    AllOf,
+    AnyOf,
+    Constraint,
+)
 from .exceptions import (
     ConstraintError,
 )

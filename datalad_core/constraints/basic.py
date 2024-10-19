2e160ca45f6751ac4b8a018ded58c34cb76b7379
Michael Hanke <michael.hanke@gmail.com>
2024-10-19 07:03:51 +0200

    feat: `EnsureMappingHasKeys` constraint

--- datalad_core/constraints/basic.py
+++ datalad_core/constraints/basic.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
+from typing import Any
+
 from datalad_core.constraints.constraint import Constraint
 
 
@@ -11,4 +14,34 @@ class NoConstraint(Constraint):
         return ''
 
     def __call__(self, value):
+        return value
+
+
+class EnsureMappingtHasKeys(Constraint):
+    """Ensure a mapping has all given keys"""
+
+    def __init__(self, required_keys: tuple | list):
+        self._required_keys = required_keys
+
+    @property
+    def input_synopsis(self):
+        return (
+            'mapping with required keys {self._required_keys!r}'
+            if self._required_keys
+            else 'mapping'
+        )
+
+    def __call__(self, value: Any) -> dict:
+        if not isinstance(value, Mapping):
+            self.raise_for(
+                value,
+                'not a mapping',
+            )
+        missing = tuple(a for a in self._required_keys if a not in value)
+        if missing:
+            self.raise_for(
+                value,
+                'missing keys {missing!r}',
+                missing=missing,
+            )
         return value
--- datalad_core/constraints/tests/test_basic.py
+++ datalad_core/constraints/tests/test_basic.py
@@ -1,4 +1,10 @@
-from datalad_core.constraints.basic import NoConstraint
+import pytest
+
+from datalad_core.constraints.basic import (
+    EnsureMappingtHasKeys,
+    NoConstraint,
+)
+from datalad_core.constraints.exceptions import ConstraintError
 
 
 def test_noconstraint():
@@ -6,3 +12,19 @@ def test_noconstraint():
     assert not c.input_synopsis
     test_val = 5
     assert c(test_val) is test_val
+
+
+def test_ensuremappinghaskeys():
+    c = EnsureMappingtHasKeys(())
+    assert c.input_synopsis == 'mapping'
+    with pytest.raises(ConstraintError, match='not a mapping'):
+        c(5)
+    test_mapping = {5: 'some', 'A': 'some', None: 'some'}
+    assert c(test_mapping) is test_mapping
+
+    c = EnsureMappingtHasKeys((None, 5))
+    assert c(test_mapping) is test_mapping
+
+    c = EnsureMappingtHasKeys(('exotic', 'bogus'))
+    with pytest.raises(ConstraintError, match='missing.*exotic.*bogus'):
+        c(test_mapping)

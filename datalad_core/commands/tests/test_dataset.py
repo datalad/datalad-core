ed73f6701f3dd1c48a78654b55b7ba0e54b17147
Michael Hanke <michael.hanke@gmail.com>
2024-10-23 09:37:53 +0200

    feat: `EnsureDataset` constraint
    A constraint to yield a `Dataset` instance from a wide range of inputs.
    
    This class is envisioned to be a standard ingredient to the majority
    of to-be-implemented commands.

--- datalad_core/commands/tests/test_dataset.py
+++ datalad_core/commands/tests/test_dataset.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
-from datalad_core.commands.dataset import Dataset
+import pytest
+
+from datalad_core.commands.dataset import (
+    Dataset,
+    EnsureDataset,
+)
+from datalad_core.config import ConfigItem
+from datalad_core.constraints import ConstraintError
 from datalad_core.repo import (
     Repo,
     Worktree,
@@ -67,3 +74,72 @@ def _assert_no_underlying_git(spec, ds):
     assert ds.worktree is None
     # under all circumstances we have a path associated with a dataset
     assert ds.path is not None
+
+
+def test_ensuredataset(tmp_path):
+    assert EnsureDataset().input_synopsis == '(path to) a dataset'
+    assert (
+        EnsureDataset(installed=True).input_synopsis == '(path to) an existing dataset'
+    )
+    assert (
+        EnsureDataset(installed=False).input_synopsis
+        == '(path to) a non-existing dataset'
+    )
+
+    with pytest.raises(
+        ConstraintError,
+        match="cannot create Dataset from <class 'int'>",
+    ):
+        EnsureDataset()(5)
+
+    with pytest.raises(
+        ConstraintError,
+        match='not installed',
+    ):
+        EnsureDataset(installed=True)(tmp_path)
+
+
+def test_ensuredataset_bare(baregitrepo):
+    c = EnsureDataset()
+    for spec in (baregitrepo,):
+        assert c(spec).repo.path == baregitrepo
+
+    with pytest.raises(
+        ConstraintError,
+        match='already exists locally',
+    ):
+        EnsureDataset(installed=False)(baregitrepo)
+
+
+def test_ensuredataset_nonbare(gitrepo):
+    c = EnsureDataset()
+    for spec in (gitrepo,):
+        assert c(spec).worktree.path == gitrepo
+
+    with pytest.raises(
+        ConstraintError,
+        match='already exists locally',
+    ):
+        EnsureDataset(installed=False)(gitrepo)
+
+
+def test_ensuredataset_with_id(tmp_path, gitrepo):
+    c = EnsureDataset(installed='with-id')
+    with pytest.raises(
+        ConstraintError,
+        match='not installed',
+    ):
+        c(tmp_path)
+
+    with pytest.raises(
+        ConstraintError,
+        match='does not have a datalad-id',
+    ):
+        c(gitrepo)
+
+    # now set an ID in the branch config and satisfy the test
+    wt = Worktree(gitrepo)
+    wt.config.sources['datalad-branch']['datalad.dataset.id'] = ConfigItem(
+        '9557edc6-910e-11ef-bcf8-23e60847c95d'
+    )
+    assert c(gitrepo).path == gitrepo
